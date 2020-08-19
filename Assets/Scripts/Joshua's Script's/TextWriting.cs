ee291c2fb509db7ec0dcd334225d08c668291621
Meraki-Destric <67037310+Meraki-Destric@users.noreply.github.com>
2020-08-19 15:55:56 +1000

    Writing Text Effect
    Creates the script for the writing text effect in the visual novel

---
+++ Assets/Scripts/Joshua's Script's/TextWriting.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextWriting : MonoBehaviour
+{
+    private Text uiText;
+    private string toBeWritten;
+    private int characterIndex;
+    private float characterSpeed;
+    private float timer;
+
+    public void TextWriter(Text uiText, string toBeWritten, float characterSpeed)
+    {
+        this.uiText = uiText;
+        this.toBeWritten = toBeWritten;
+        this.characterSpeed = characterSpeed;
+        characterIndex = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (uiText != null)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0f)
+            {
+                timer += characterSpeed;
+                characterIndex++;
+                uiText.text = toBeWritten.Substring(0, characterIndex);
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Joshua's Script's/UIAssistant.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor.VersionControl;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] private TextWriting textWriting;
+    private Text messageText;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //Creates a reference for a textfield
+        //First string finds the location of the object with the same name, second one finds a reference to the object inside with the inserted name
+        messageText = transform.Find("message").Find("messageText").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Start()
+    {
+        textWriting.TextWriter(messageText, "Hello World", 0.1f);
+    }
+}
