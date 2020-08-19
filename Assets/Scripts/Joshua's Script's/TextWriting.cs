3a8f69a7cdd4662aaa4fb59e178cfae7085c528d
Meraki-Destric <67037310+Meraki-Destric@users.noreply.github.com>
2020-08-20 00:00:42 +1000

    More work on VN Stuff
    Currently still trying to get the writing text to work

--- Assets/Scripts/Joshua's Script's/TextWriting.cs
+++ Assets/Scripts/Joshua's Script's/TextWriting.cs
@@ -1,9 +1,29 @@
-﻿using System.Collections;
+﻿using JetBrains.Annotations;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TextWriting : MonoBehaviour
+{
+    private TextWriterSingle textWriterSingle;
+    public void TextWriter(Text uiText, string toBeWritten, float characterSpeed, bool invisibleCharacters)
+    {
+        textWriterSingle = new TextWriterSingle(uiText, toBeWritten, characterSpeed, invisibleCharacters);
+    
+    }
+
+    private void Update()
+    {
+        if (textWriterSingle != null)
+        {
+            textWriterSingle.Update();
+        }
+    }
+
+}
+
+public class TextWriterSingle
 {
     private Text uiText;
     private string toBeWritten;
@@ -11,28 +31,43 @@ public class TextWriting : MonoBehaviour
     private float characterSpeed;
     private float timer;
 
-    public void TextWriter(Text uiText, string toBeWritten, float characterSpeed)
+    //Essentially writes out the text before displaying it so that each letter is rendered in it's spot
+    private bool invisibleCharacters;
+
+    public TextWriterSingle(Text uiText, string toBeWritten, float characterSpeed, bool invisibleCharacters)
     {
         this.uiText = uiText;
         this.toBeWritten = toBeWritten;
         this.characterSpeed = characterSpeed;
+        this.invisibleCharacters = invisibleCharacters;
         characterIndex = 0;
-
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
         if (uiText != null)
         {
             timer -= Time.deltaTime;
-            if (timer <= 0f)
+            while (timer <= 0f)
             {
                 timer += characterSpeed;
                 characterIndex++;
-                uiText.text = toBeWritten.Substring(0, characterIndex);
+                string text = toBeWritten.Substring(0, characterIndex);
 
+                if (invisibleCharacters)
+                {
+                    text += "<color = #00000000>" + toBeWritten.Substring(characterIndex) + "</color>";
+                }
+
+                uiText.text = text;
+
+                if (characterIndex >= toBeWritten.Length)
+                {
+                    uiText = null;
+                    return;
+                }
             }
         }
     }
-}
+};
--- Assets/Scripts/Joshua's Script's/UIAssistant.cs
+++ Assets/Scripts/Joshua's Script's/UIAssistant.cs
@@ -5,22 +5,29 @@ using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class UIAssistant : MonoBehaviour
 {
     [SerializeField] private TextWriting textWriting;
     private Text messageText;
+    private Text nameText;
 
     // Start is called before the first frame update
     void Awake()
     {
         //Creates a reference for a textfield
         //First string finds the location of the object with the same name, second one finds a reference to the object inside with the inserted name
-        messageText = transform.Find("message").Find("messageText").GetComponent<Text>();
+        messageText = transform.Find("Text Box").Find("Story").GetComponent<Text>();
+        nameText = transform.Find("Text Box").Find("Name").GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Start()
     {
-        textWriting.TextWriter(messageText, "Hello World", 0.1f);
+
+        textWriting.TextWriter(messageText, "The mumbling of voices vaguely reaches their ears. Some loud" +
+            ", some soft. Yet utterly indescernible", 0.1f, true);
+        textWriting.TextWriter(nameText, "", 0f, true);
+
     }
 }
+
